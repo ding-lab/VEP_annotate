42d49ee4d56d300bcc2fe534e3d563b435bb4c6a
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-06-07 17:29:49 -0500

    Successful run on docker

--- testing/docker_call/1_run_vep_annotate-cache_dir.sh
+++ testing/docker_call/1_run_vep_annotate-cache_dir.sh
@@ -15,23 +15,23 @@ MOUNT="$VEP_ROOT:/vep"
 MOUNT="$MOUNT $REF_ROOT:/ref"
 MOUNT="$MOUNT $DATA_ROOT:/data"
 
-OUTD="/data/vep_annotate_docker"
-mkdir -p $OUTD
-
 REF="/ref/GRCh38.d1.vd1/GRCh38.d1.vd1.fa"
 VCF="/data/merged/filtered.vcf"
 VEP_GZ="/vep/v99/vep-cache.99_GRCh38.tar.gz"
+VEP_DIR="/vep"
 
 VEP_VER="99"
 ASSEMBLY="GRCh38"
 
 #INPUTS="--input_vcf $VCF --reference_fasta $REF --assembly $ASSEMBLY --vep_cache_version $VEP_VER --vep_cache_gz $VEP_GZ"
 # no DB version below, so that ./vep-cache not created (use less disk space)
-INPUTS="--input_vcf $VCF --reference_fasta $REF --assembly $ASSEMBLY --vep_cache_version $VEP_VER"
+INPUTS="--input_vcf $VCF --reference_fasta $REF --assembly $ASSEMBLY --vep_cache_version $VEP_VER --vep_cache_dir $VEP_DIR"
 
 ARGS="--results_dir results --vep_opts \"--hgvs --shift_hgvs 1 --no_escape --symbol --numbers --ccds --uniprot --xref_refseq --sift b --tsl --canonical --total_length --allele_number --variant_class --biotype --appris --flag_pick_allele --check_existing --failed 1 --minimal --pick_order biotype,rank,canonical\""
+#ARGS="--results_dir results "
 
-#CMD="/usr/bin/perl /usr/local/TinDaisy-VEP/SomaticWrapper.pl $INPUTS $ARGS"
+ARGS=$(echo "$ARGS" | sed 's/"/\\"/g' ) # escape quotes
+
 CMD="/usr/bin/perl /usr/local/TinDaisy-VEP/SomaticWrapper.pl $INPUTS $ARGS"
 
 ARGS="-M docker -l"
