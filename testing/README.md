d1fb1b85b5b198108243788058b18b2dce003113
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2022-05-25 14:50:26 -0500

    Preliminary updates of VEP testing scripts
    * Removing support for MGI testing
    * Cromwell testing with filedb

--- testing/README.md
+++ testing/README.md
@@ -2,5 +2,5 @@ Several levels of calls available for testing
 
 - direct\_call: Calling directly from within container
 - docker\_call: Instantiate docker container and call scripts within it
-- cwl\_call: Run rabix or Cromwell workflow manager to call CWL workflow
+- cwl\_call: Run Cromwell to execute CWL workflow
 
---
+++ testing/cwl_call-compute1/1_run_cromwell_demo.sh
@@ -0,0 +1,30 @@
+# Be sure this is running within cromwell-compatible docker on compute1 with,
+# 0_start_docker-compute1_cromwell.sh
+
+source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
+
+PWD=$(pwd)
+CWL_ROOT_H=$PWD/../..
+CWL="$CWL_ROOT_H/cwl/XXX.cwl"
+
+CONFIG="dat/cromwell-config-db.compute1-filedb.dat"
+YAML="dat/XXX.yaml"
+
+# Cromwell v78 
+JAVA="/opt/java/openjdk/bin/java"
+CROMWELL="/app/cromwell-78-38cd360.jar"
+
+# from https://confluence.ris.wustl.edu/pages/viewpage.action?spaceKey=CI&title=Cromwell#Cromwell-ConnectingtotheDatabase
+# Connecting to the database section
+# Note also database section in config file
+CMD="$JAVA -Dconfig.file=$CONFIG -jar $CROMWELL run -t cwl -i $YAML $CWL"
+
+echo Running: $CMD
+eval $CMD
+
+rc=$?
+if [[ $rc != 0 ]]; then
+    >&2 echo Fatal error $rc: $!.  Exiting.
+    exit $rc;
+fi
+
